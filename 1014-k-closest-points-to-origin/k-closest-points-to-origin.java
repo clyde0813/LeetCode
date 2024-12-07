251280c383fa5bd46cdc29506b485dcc44d40c22
ClydeJIJI <76196439+clyde0813@users.noreply.github.com>
2024-12-07 18:15:41 +0900

    Time: 26 ms (71.37%) | Memory: 52.4 MB (60.22%) - LeetSync

---
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -0,0 +1,22 @@
+class Solution {
+    static class Point{
+        double distance;
+        int[] point;
+        public Point(double distance, int[] point){
+            this.distance = distance;
+            this.point = point;
+        }
+    }
+    public int[][] kClosest(int[][] points, int k) {
+        int[][] results = new int[k][];
+        PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparingDouble(point -> point.distance));
+        for(int[] point : points){
+            double distance = (long) point[0] * point[0] + (long) point[1] * point[1];
+            pq.add(new Point(distance, point));
+        }
+        for(int i=0;i<k;i++){
+            results[i] = pq.poll().point;
+        }
+        return results;
+    }
+}
