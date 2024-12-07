3ee969b7a8b90ae59437f349f4854d38fb97c7f7
ClydeJIJI <76196439+clyde0813@users.noreply.github.com>
2024-12-07 18:18:09 +0900

    Time: 27 ms (69.42%) | Memory: 52.2 MB (60.96%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -11,6 +11,7 @@ class Solution {
         int[][] results = new int[k][];
         PriorityQueue<Point> pq = new PriorityQueue<>(Comparator.comparingDouble(point -> point.distance));
         for(int[] point : points){
+            // Omitting the square root calculation, as it's unnecessary for distance ranking
             double distance = (long) point[0] * point[0] + (long) point[1] * point[1];
             pq.add(new Point(distance, point));
         }
