95745f9635ede8ab15ba0fdfa23d1d437d0a9b3b
ClydeJIJI <76196439+clyde0813@users.noreply.github.com>
2024-12-08 18:07:50 +0900

    Time: 1 ms (61.77%) | Memory: 42 MB (22.01%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -1,18 +1,12 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int result = 0;
-        Map<Character, Integer> freqs = new HashMap<>();
-        for(char c : stones.toCharArray()){
-            if(freqs.containsKey(c)){
-                freqs.put(c, freqs.get(c) + 1);
-            } else {
-                freqs.put(c, 1);
-            }
-        }
+        Set<Character> freqs = new HashSet<>();
         for(char c : jewels.toCharArray()){
-            if(freqs.containsKey(c)){
-                result += freqs.get(c);
-            }
+            freqs.add(c);
+        }
+        for(char c : stones.toCharArray()){
+            if(freqs.contains(c)) result++;
         }
         return result;
     }
