ac79a04f87fa17e8ac2a6c08690cfca8282e6700
ClydeJIJI <76196439+clyde0813@users.noreply.github.com>
2024-12-20 20:15:34 +0900

    Time: 3 ms (27.93%) | Memory: 44.6 MB (46.89%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,22 +1,21 @@
 class Solution {
-    public void dfs(List<List<Integer>> results, int[] candidates, Deque<Integer> elements, int target, int index){
+    public void dfs(List<List<Integer>> results, int[] candidates, Deque<Integer> elements, int target, int index, int cLength){
+        if(target < 0) return;
         if(target == 0){
             results.add(new ArrayList<>(elements));
             return;
         }
-        if(target < 0){
-            return;
-        }
-        for(int i=index; i<candidates.length; i++){
+        for(int i=index; i<cLength; i++){
             elements.add(candidates[i]);
-            dfs(results, candidates, elements, target - candidates[i], i);
+            dfs(results, candidates, elements, target - candidates[i], i, cLength);
             elements.removeLast();
         }
 
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> results = new ArrayList<>();
-        dfs(results, candidates, new ArrayDeque<>(), target, 0);
+        int cLength = candidates.length;
+        dfs(results, candidates, new ArrayDeque<>(), target, 0, cLength);
         return results;
     }
 }
