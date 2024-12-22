cef0f85f5c19d6f4e9dfcf5e3171d2b41907a698
ClydeJIJI <76196439+clyde0813@users.noreply.github.com>
2024-12-22 14:52:45 +0900

    Time: 88 ms (5.07%) | Memory: 46 MB (18.40%) - LeetSync

---
+++ 207-course-schedule/course-schedule.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean dfs(Map<Integer, List<Integer>> courseToMap, Integer start, List<Integer> takes, List<Integer> taken){
+        if(takes.contains(start)) return false;
+        if(taken.contains(start)) return true;
+        if(courseToMap.containsKey(start)){
+            takes.add(start);
+            for(Integer course : courseToMap.get(start)){
+                if(!dfs(courseToMap, course, takes, taken)) return false;
+            }
+            takes.remove(start);
+            taken.add(start);
+        }
+        return true;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> courseToMap = new HashMap<>();
+        List<Integer> takes = new ArrayList<>();
+        List<Integer> taken = new ArrayList<>();
+        for(int[] pre : prerequisites){
+            courseToMap.putIfAbsent(pre[0], new ArrayList<>());
+            courseToMap.get(pre[0]).add(pre[1]);
+        }
+        for(Integer i : courseToMap.keySet()){
+            if(!dfs(courseToMap, i, takes, taken)) return false;
+        }
+        return true;
+    }
+}
