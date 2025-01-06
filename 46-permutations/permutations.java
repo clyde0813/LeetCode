be80b64a7e42edf4d6900f8619546d1b40e6aa29
ClydeJIJI <76196439+clyde0813@users.noreply.github.com>
2025-01-06 19:27:52 +0900

    Time: 5 ms (2.74%) | Memory: 44.6 MB (38.07%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,9 +1,15 @@
 class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        List<Integer> lst = Arrays.stream(nums).boxed().collect(Collectors.toList());
+        dfs(results, new ArrayList<>(), lst);
+        return results;
+    }
+
     public void dfs(List<List<Integer>> results, List<Integer> prevElements, List<Integer> elements){
         if(elements.isEmpty()){
-            results.add(new ArrayList<>(prevElements));
+            results.add(prevElements.stream().collect(Collectors.toList()));
         }
-
         for(Integer e : elements){
             List<Integer> nextElements = new ArrayList<>(elements);
             nextElements.remove(e);
@@ -11,11 +17,5 @@ class Solution {
             dfs(results, prevElements, nextElements);
             prevElements.remove(e);
         }
-    }
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> results = new ArrayList<>();
-        List<Integer> numList = Arrays.stream(nums).boxed().collect(Collectors.toList());
-        dfs(results, new ArrayList<>(), numList);
-        return results;
     }
 }
