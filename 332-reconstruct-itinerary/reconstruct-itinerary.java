bf1461c1e7092a8dd6a66392e49c03fa0436f03d
ClydeJIJI <76196439+clyde0813@users.noreply.github.com>
2025-01-20 19:42:02 +0900

    Time: 6 ms (64.04%) | Memory: 45.5 MB (60.35%) - LeetSync

--- 332-reconstruct-itinerary/reconstruct-itinerary.java
+++ 332-reconstruct-itinerary/reconstruct-itinerary.java
@@ -1,16 +1,18 @@
 class Solution {
     public void dfs(List<String> results, Map<String, PriorityQueue<String>> fromToMap, String from){
-        while(fromToMap.containsKey(from)&&!fromToMap.get(from).isEmpty()){
+        while(fromToMap.containsKey(from) && !fromToMap.get(from).isEmpty()){
             dfs(results, fromToMap, fromToMap.get(from).poll());
         }
         results.add(0, from);
     }
+
     public List<String> findItinerary(List<List<String>> tickets) {
         List<String> results = new ArrayList<>();
         Map<String, PriorityQueue<String>> fromToMap = new HashMap<>();
-        for(List<String> ticketList : tickets){
-            fromToMap.putIfAbsent(ticketList.get(0), new PriorityQueue<>());
-            fromToMap.get(ticketList.get(0)).add(ticketList.get(1));
+
+        for(List<String> ticket : tickets){
+            fromToMap.putIfAbsent(ticket.get(0), new PriorityQueue<>());
+            fromToMap.get(ticket.get(0)).add(ticket.get(1));
         }
         dfs(results, fromToMap, "JFK");
         return results;
