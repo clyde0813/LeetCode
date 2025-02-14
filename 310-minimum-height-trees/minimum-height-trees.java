3394f3271e2019b3a3a6462cca40d1d3da9879e2
ClydeJIJI <76196439+clyde0813@users.noreply.github.com>
2025-02-14 10:32:42 +0900

    Time: 49 ms (11.85%) | Memory: 56.1 MB (63.71%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if(n==1) return new ArrayList<>(List.of(0));
+        if(n==1) return List.of(0);
         Map<Integer, List<Integer>> graph = new HashMap<>();
         for(int[] edge : edges){
             graph.putIfAbsent(edge[0], new ArrayList<>());
